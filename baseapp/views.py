cc7ea8057439332d81c7764c8743f35e04d5ab41
Enes Görkem Genç <gencenesgorkem@outlook.com>
2022-06-22 02:37:04 +0300

    Added front-end to the chatbot, a chat page and the data table page.

--- baseapp/views.py
+++ baseapp/views.py
@@ -9,11 +9,20 @@ import sqlite3
 
 
 def get_homepage(request):
+    
+    return render(request, "base/index.html")
+
+
+
+
+def get_data_page(request):
+
     connection = sqlite3.connect("chat.db")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM tbl_chat")
-    table_data = {req:res for (req, res) in cursor.fetchall()}
+    data = {req:res for (req, res) in cursor.fetchall()}
     
     connection.close()
-    context = {"data": table_data}
-    return render(request, "temporary.html", context)
+
+    context = {"chat_data": data}
+    return render(request, "base/data.html", context)
